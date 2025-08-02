bee27609b18d44d6cf2815fb4fdffeef2e4940d9
Sr-Yagame <ghdyagame74@gmail.com>
2025-08-02 02:23:38 -0300

    Delete api-search directory

--- api-search/utils/firebase.js
+++
@@ -1,9 +0,0 @@
-import { initializeApp } from 'firebase/app';
-import { getDatabase } from 'firebase/database';
-
-const firebaseConfig = {
-  databaseURL: process.env.FIREBASE_DB_URL
-};
-
-const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
