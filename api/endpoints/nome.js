9086608d3d3adf2bd4596443fbcdf428d31a3cbe
Sr-Yagame <ghdyagame74@gmail.com>
2025-08-03 03:54:34 -0300

    Update nome.js

--- api/endpoints/nome.js
+++ api/endpoints/nome.js
@@ -10,7 +10,6 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', process.env.UL || '');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Max-Age', '86400');
 
   if (req.method === 'OPTIONS') {
     return res.status(204).end();
